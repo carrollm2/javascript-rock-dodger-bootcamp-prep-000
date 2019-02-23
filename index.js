7150b1eb8735f66026f2810d3741f221eb0edbd2
Michael Carroll <carrollm2@gmail.com>
2019-02-23 22:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function createRock(x) {
      if (top < 400){
        window.requestAnimationFrame(moveRock)
      } else {
-       GAME.removeChild(rock)
+       GAME.removeChild(document.getElementByClassName("rock"))
      }
 
   }
